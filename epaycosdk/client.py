06dc392e0c97ca1759605e4e10a4d6836f810e9f
Gerson Vasquez <gerson.vasquez@epayco.com>
2021-12-30 09:59:26 -0400

    Arreglado variable APIFY

--- epaycosdk/client.py
+++ epaycosdk/client.py
@@ -110,7 +110,7 @@ class Client:
     BASE_URL = os.getenv("BASE_URL_SDK") if os.getenv("BASE_URL_SDK") else "https://api.secure.payco.co"
     BASE_URL_SECURE = os.getenv("SECURE_URL_SDK") if os.getenv("SECURE_URL_SDK") else"https://secure.payco.co"
     ENTORNO = os.getenv("ENTORNO_SDK") if os.getenv("ENTORNO_SDK") else "/restpagos"
-    BASE_URL_APIFY = os.getenv("BASE_URL_APIF") if os.getenv("BASE_URL_APIF") else "https://apify.epayco.co"
+    BASE_URL_APIFY = os.getenv("BASE_URL_APIFY") if os.getenv("BASE_URL_APIFY") else "https://apify.epayco.co"
     IV = "0000000000000000"
     LANGUAGE = "python"
     SWITCH= False
