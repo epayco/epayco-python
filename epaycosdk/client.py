2366a1902e8c41cf6a61478649c668ee58f4751b
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2022-04-05 09:46:38 -0500

    fix: se eliminan codigo comentado, se realiza correcion en valdiacion de metodo de pago con pse y tc para procesar el pago de forma correcta

--- epaycosdk/client.py
+++ epaycosdk/client.py
@@ -26,8 +26,6 @@ unpad = lambda s : s[0:-(s[-1])]
 BASE_DIR = Path(__file__).resolve().parent.parent
 EPAYCO_KEY_LANG_FILE = str(BASE_DIR.joinpath('epaycosdk/utils/key_lang.json'))
 EPAYCO_KEY_LANG_FILE_APIFY = str(BASE_DIR.joinpath('epaycosdk/utils/key_lang_apify.json'))
-#Dir = os.path.join(EPAYCO_KEY_LANG_FILE, 'key_lang.json')
-
 
 class AESCipher:
     def __init__( self, key,iv  ):
@@ -39,8 +37,6 @@ class AESCipher:
         cipher = AES.new( self.key.encode("utf8"), AES.MODE_CBC, self.iv.encode("utf8"))
         enc = cipher.encrypt(raw)
         return base64.b64encode(enc)
-        #         cipher = AES.new( raw.encode("utf8"), AES.MODE_CBC, iv().encode("utf8") )
-        # return base64.b64encode(cipher.encrypt( pad(raw) ) ).strip()
     def decrypt( self, enc ):
        
         enc = base64.b64decode(enc)
@@ -104,8 +100,6 @@ class Auth:
             payload = ""
         response = requests.request("POST", url, headers=headers, data = payload)
         data=response.text.encode('utf8')
-        # print(data)
-        # sys.exit()
         json_data=json.loads(data)
         bearer_token=json_data['token'] if apify else json_data['bearer_token']
         return bearer_token
@@ -129,10 +123,7 @@ class Client:
     SWITCH= False
 
     def __init__(self):
-
         pass
-
-
 
     """
     Make request and return a Python object from the JSON response. If
@@ -157,11 +148,11 @@ class Client:
         if(apify):
             data = util.setKeys_apify(data)
         elif (hasattr(data, "__len__")):
-            data = util.setKeys(data)
+            if(switch):
+                data = util.setKeys(data)
 
         self.SWITCH=switch  
         self.APIFY=apify
-        #headers = {'Content-Type':'application/json','Accept' : "application/json" ,'type':'sdk-jwt'}
         headers = {
             'Content-Type': 'application/json',
             'type': 'sdk-jwt',
@@ -180,18 +171,14 @@ class Client:
                     #Encriptamos el enpruebas
                     aes = AESCipher(private_key,self.IV)
                     enpruebas=aes.encrypt(test)
-
                     addData = {
                         'public_key': api_key,
                         'i': base64.b64encode(self.IV.encode('ascii')),
                         'lenguaje': self.LANGUAGE,
                         'enpruebas': enpruebas,
                     }
-
                     url_params = addData
                     url_params.update(data)
-                    # print('get')
-                    # sys.exit()
                     response=requests.get(self.build_url(url), data={},params=url_params,auth=(api_key, ""),headers=headers)
 
                 else:
@@ -208,60 +195,28 @@ class Client:
                     else:
                         test= "FALSE"
    
+                    aes = AESCipher(private_key, self.IV)
+                    enpruebas = aes.encrypt(test)
                     if(cashdata):
-                        aes = AESCipher(private_key,self.IV)
-                        enpruebas = aes.encrypt(test)
-
-                        encryptData = data
-                        #encryptData = aes.encryptArray(data)
-
-                        addData = {
-                            "public_key": api_key,
-                            "i": base64.b64encode(self.IV.encode('ascii')),
-                            "enpruebas": enpruebas,
-                            "lenguaje": self.LANGUAGE,
-                            "p": ""
-                        }
-                        enddata = {}
-                        enddata.update(encryptData)
-                        enddata.update(addData)
-                        data=enddata
-                        #payload = "{\"factura\":\""+encryptData['factura']+"\",\"descripcion\":\""+encryptData['descripcion']+"\",\"valor\":\""+encryptData['valor']+"\",\"iva\":\""+encryptData['iva']+"\",\"baseiva\":\""+encryptData['baseiva']+"\",\"moneda\":\""+encryptData['moneda']+"\",\"tipo_persona\":\""+encryptData['tipo_persona']+"\",\"tipo_doc\":\""+encryptData['tipo_doc']+"\",\"documento\":\""+encryptData['documento']+"\",\"nombres\":\""+encryptData['nombres']+"\",\"apellidos\":\""+encryptData['apellidos']+"\",\"email\":\""+encryptData['email']+"\",\"pais\":\""+encryptData['pais']+"\",\"depto\":\""+encryptData['depto']+"\",\"ciudad\":\""+encryptData['ciudad']+"\",\"celular\":\""+encryptData['celular']+"\",\"direccion\":\""+encryptData['direccion']+"\",\"ip\":\""+encryptData['ip']+"\",\"url_respuesta\":\""+encryptData['url_respuesta']+"\",\"url_confirmacion\":\""+encryptData['url_confirmacion']+"\",\"metodoconfirmacion\":\""+encryptData['metodoconfirmacion']+"\",\"fechaexpiracion\":\""+encryptData['fechaexpiracion']+"\",\"test\":\""+test+"\",\"public_key\":\""+api_key+"\",\"i\":\"MDAwMDAwMDAwMDAwMDAwMA==\",\"enpruebas\":\"''\",\"lenguaje\":\"python\",\"p\":\"\"}"
-                        #response = requests.request("POST", self.build_url(url), headers=headers, data = payload)
-                        response = requests.post(self.build_url(url),params=data, auth=(api_key, ''),headers=headers)
-                        # print(response)
-                        # sys.exit()
-
-
-                       
+                        encryptData = data  
                     else:
-                        aes = AESCipher(private_key,self.IV)
-                        enpruebas = aes.encrypt(test)
-                        encryptData = None
                         encryptData = aes.encryptArray(data)
-                        addData = {
-                            'public_key': api_key,
-                            'i': base64.b64encode(self.IV.encode('ascii')),
-                            'enpruebas': enpruebas,
-                            'lenguaje': self.LANGUAGE,
-                            'p': ''
-                        }
 
-                        enddata = {}
-                        enddata.update(encryptData)
-                        enddata.update(addData)
-                        data=enddata
-                        #payload = "{\"banco\":\""+str(encryptData['banco'])+"\",\"factura\":\""+str(encryptData['factura'])+"\",\"descripcion\":\""+str(encryptData['descripcion'])+"\",\"valor\":\""+str(encryptData['valor'])+"\",\"iva\":\""+str(encryptData['iva'])+"\",\"baseiva\":\""+str(encryptData['baseiva'])+"\",\"moneda\":\""+str(encryptData['moneda'])+"\",\"tipo_persona\":\""+str(encryptData['tipo_persona'])+"\",\"tipo_doc\":\""+str(encryptData['tipo_doc'])+"\",\"documento\":\""+str(encryptData['documento'])+"\",\"nombres\":\""+str(encryptData['nombres'])+"\",\"apellidos\":\""+str(encryptData['apellidos'])+"\",\"email\":\""+str(encryptData['email'])+"\",\"pais\":\""+str(encryptData['pais'])+"\",\"celular\":\""+str(encryptData['celular'])+"\",\"url_respuesta\":\""+str(encryptData['url_respuesta'])+"\",\"url_confirmacion\":\""+str(encryptData['url_confirmacion'])+"\",\"metodoconfirmacion\":\""+str(encryptData['metodoconfirmacion'])+"\",\"ip\":\""+str(encryptData['ip'])+"\",\"test\":\""+test+"\",\"public_key\":\""+api_key+"\",\"i\":\""+str(addData['i'])+"\",\"enpruebas\":\""+str(addData['enpruebas'])+"\",\"lenguaje\":\"python\",\"p\":\"\"}"
-                        response = requests.post(self.build_url(url),params=data, auth=(api_key, ''),headers=headers)
-                        #response = requests.request("POST", self.build_url(url), headers=headers, data = data)
-                        # print(response)
-                        # sys.exit()
-
-
+                    addData = {
+                        'public_key': api_key,
+                        'i': base64.b64encode(self.IV.encode('ascii')),
+                        'enpruebas': enpruebas,
+                        'lenguaje': self.LANGUAGE,
+                        'p': ''
+                    }
+                    enddata = {}
+                    enddata.update(encryptData)
+                    enddata.update(addData)
+                    data=enddata
+                    response = requests.post(self.build_url(url),params=data, auth=(api_key, ''),headers=headers)
 
                 else:
                     #Agregamos la llave publica
-                    #data.update({'public_key':api_key,'test': test})
                     if(dt):
                         data=json.dumps(data)
                         response = requests.request("POST", self.build_url(url), headers=headers, data = data)
@@ -300,21 +255,16 @@ class Client:
             raise errors.ErrorException(lang, 103)
 
         if (response.status_code == 401):
-
             raise errors.ErrorException(lang, 104)
 
         if (response.status_code == 404):
-
             raise errors.ErrorException(lang, 105)
 
         if (response.status_code == 403):
-
             raise errors.ErrorException(lang, 106)
 
         if (response.status_code == 405):
-
             raise errors.ErrorException(lang, 107)
-
 
         raise errors.ErrorException(lang, 102)
 
