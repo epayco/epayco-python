5124de18553db76a4a00c3cca5af7fe645c24ce1
Gerson Vasquez <gerson.vasquez@epayco.com>
2022-04-22 12:44:20 -0400

    Se actualiza version de sdk a v3.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import platform
 
 setup(
     name="epaycosdk",
-    version="3.1.0",
+    version="3.1.1",
     include_package_data=True,
     author="ePayco Development Team",
     author_email="ricardo.saldarriaga@epayco.co",
