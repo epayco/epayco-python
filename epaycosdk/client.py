a83f4ae22547c42847e40469d3e80a63eb94a69c
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-05-16 15:37:29 -0500

    fix:Se ajusta endpoint de rest pagos

--- epaycosdk/client.py
+++ epaycosdk/client.py
@@ -93,7 +93,7 @@ class NoRebuildAuthSession(Session):
 class Client:
 
     BASE_URL = os.getenv("BASE_URL_SDK") if os.getenv("BASE_URL_SDK") else "https://eks-subscription-api-lumen-service.epayco.io"
-    BASE_URL_SECURE = os.getenv("SECURE_URL_SDK") if os.getenv("SECURE_URL_SDK") else "https://eks-rest-recaudo-service.epayco.io"
+    BASE_URL_SECURE = os.getenv("SECURE_URL_SDK") if os.getenv("SECURE_URL_SDK") else "https://eks-rest-pagos-service.epayco.io"
     ENTORNO = os.getenv("ENTORNO_SDK") if os.getenv("ENTORNO_SDK") else "/restpagos"
     BASE_URL_APIFY = os.getenv("BASE_URL_APIFY") if os.getenv("BASE_URL_APIFY") else "https://eks-apify-service.epayco.io"
     IV = "0000000000000000"
